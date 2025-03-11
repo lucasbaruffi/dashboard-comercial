74df8cbbb5e0c3182e923cff05e8d4c78bdc9957
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-03-11 09:37:34 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from dotenv import load_dotenv, set_key
+from requests_oauthlib import OAuth2Session
+
 
 # Carrega os dados do .env
 load_dotenv()
@@ -10,4 +12,37 @@ clientId = os.getenv("CLIENT_ID")
 clientSecret = os.getenv("CLIENT_SECRET")
 authUrl = os.getenv("AUTH_URL")
 tokenUrl = os.getenv("TOKEN_URL")
+redirectUrl = os.getenv("REDIRECT_URL")
 
+# Define os Escopos:
+scope = [
+    "opportunities.readonly",
+    "contacts.readonly",
+    "calendars/events.write",
+    "calendars/events.readonly",
+    "locations.readonly",
+    "calendars.write",
+    "calendars.readonly"
+]
+
+# Cria a sessão OAuth
+oauth = OAuth2Session(clientId=scope, redirect_uri=redirectUrl)
+
+# 1) Obter a URL de autorização e redirecionar o usuário
+authorization_url, state = oauth.authorization_url(authUrl)
+print("Acesse a URL para autorizar a aplicação:")
+print(authorization_url)
+
+# 2) Após o usuário autorizar, ele será redirecionado para o redirect_uri.
+#    Copie a URL de redirecionamento (que contém o 'code') e cole aqui.
+redirect_response = input("Cole aqui a URL completa de redirecionamento:\n")
+
+# 3) Trocar o 'code' pelo 'access token'
+token = oauth.fetch_token(
+    token_url=token_url,
+    authorization_response=redirect_response,
+    client_secret=client_secret
+)
+
+print("Token obtido com sucesso:")
+print(token)
