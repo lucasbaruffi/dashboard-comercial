3be1f21e188a3ab85c3e173a5a1c076325629948
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-03-11 09:15:30 -0300

    configuração variáveis de ambiente

--- auth.py
+++ auth.py
@@ -1,2 +1,13 @@
 import requests
+import os
+from dotenv import load_dotenv, set_key
+
+# Carrega os dados do .env
+load_dotenv()
+
+# Puxa as variáveis de ambiente e salva em variáveis locais
+clientId = os.getenv("CLIENT_ID")
+clientSecret = os.getenv("CLIENT_SECRET")
+authUrl = os.getenv("AUTH_URL")
+tokenUrl = os.getenv("TOKEN_URL")
 
