903585a09f59a66777b26017900533e5166b7fc9
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-03-13 08:10:15 -0300

    correção do definePeriodo e começo do getCalendarEvents
    alterei o definePeriodo para transformar em milisegunos, antes estava em segundos (foi só multiplicar por mil) e começei a testar a requisição de agendas. Parece estar funcionando

--- main.py
+++ main.py
@@ -57,9 +57,9 @@ def definePeriodo(diasAntes: int = 30, diasDepois: int = 15):
         dataDiasAntes = data_atual - timedelta(days=diasAntes)
         dataDiasDepois = data_atual + timedelta(days=diasDepois)
 
-        # Converte para o formato Epoch (timestamp)
-        epoch_x_dias_antes = int(dataDiasAntes.timestamp())
-        epoch_x_dias_depois = int(dataDiasDepois.timestamp())
+        # Converte para o formato Epoch (timestamp) -> *1000 para transformar em milisegundos
+        epoch_x_dias_antes = int(dataDiasAntes.timestamp() * 1000)
+        epoch_x_dias_depois = int(dataDiasDepois.timestamp() * 1000)
 
         return epoch_x_dias_antes, epoch_x_dias_depois
 
@@ -68,58 +68,80 @@ def definePeriodo(diasAntes: int = 30, diasDepois: int = 15):
 
 
 def getCalendarEvents():
+    try:
+        # Define o Início e Fim 
+        startTime, endTime = definePeriodo()
+
+        # Define o Endpoint da API
+        url = "https://services.leadconnectorhq.com/calendars/events"
+
+        # Define os parâmetros necessários para a requisição
+        params = {
+            "locationId": locationId,
+            "calendarId": calendarId,
+            "startTime": startTime,
+            "endTime": endTime
+        }
+
+        # Faz a requisição
+        r = requests.get(url=url, params=params, headers=header)
+
+        # Se a resposta tiver um status code de erro, raise_for_status() vai disparar uma exceção HTTPError
+        r.raise_for_status()
+
+        # Transforma a resposta em Json e retorna
+        r = r.json()     
+
+        print(r)
+
+
+
+        return r
     
-    startTime, endTime = definePeriodo()
-
-    url = "https://services.leadconnectorhq.com/calendars/events"
-
-    params = {
-        "locationId": locationId,
-        "calendarId": calendarId,
-        "startTime": startTime,
-        "endTime": endTime
-    }
-
-    r = requests.get(url=url, params=params, headers=header)
-
-    statusCode = r.status_code
-
-    # Erro por token expirado: 401
-
-    if statusCode == 200:
-        r = r.json()        
-        return r["events"]
-    else:
-        r = r.text
-        print(r)    
 
 
 
+    except requests.exceptions.HTTPError as http_err:
+        # Exemplo: tratamento específico para token expirado (status 401)
+        if r.status_code == 401:
+            print("Erro 401: Token expirado.")
+        else:
+            print(f"Erro HTTP: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Erro na requisição: {req_err}")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
 
 
 
+getCalendarEvents()
 
-from sheets import adicionarAgendamento
 
-#eventos = getCalendarEvents()
-
-for evento in eventos:
-    dados = [
-        evento.get("id", ""),
-        evento.get("address", ""),
-        evento.get("title", ""),
-        evento.get("calendarId", ""),
-        evento.get("locationId", ""),
-        evento.get("contactId", ""),
-        evento.get("groupId", ""),
-        evento.get("appointmentStatus", ""),
-        evento.get("assignedUserId", ""),
-        evento.get("startTime", ""),
-        evento.get("endTime", ""),
-        evento.get("dateAdded", ""),
-        evento.get("dateUpdated", ""),
-        evento.get("createdBy", {}).get("userId", ""),
-        evento.get("opportunityId", "")
-    ]   
-
-    #adicionarAgendamento(dados)
+# 
+# 
+# 
+# rom sheets import adicionarAgendamento
+# 
+# eventos = getCalendarEvents()
+# 
+# or evento in eventos:
+#    dados = [
+#        evento.get("id", ""),
+#        evento.get("address", ""),
+#        evento.get("title", ""),
+#        evento.get("calendarId", ""),
+#        evento.get("locationId", ""),
+#        evento.get("contactId", ""),
+#        evento.get("groupId", ""),
+#        evento.get("appointmentStatus", ""),
+#        evento.get("assignedUserId", ""),
+#        evento.get("startTime", ""),
+#        evento.get("endTime", ""),
+#        evento.get("dateAdded", ""),
+#        evento.get("dateUpdated", ""),
+#        evento.get("createdBy", {}).get("userId", ""),
+#        evento.get("opportunityId", "")
+#    ]   
+# 
+#    #adicionarAgendamento(dados)
+# 
