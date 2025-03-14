e006748ca1783dfbd44d39b800f6b859b478fdd3
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-03-14 08:56:36 -0300

    getOpportunities
    Fiz a função getOpportunities() que faz a consulta de todas as oportunidades do GHL e deixa em um dicionário.
    
    Próximo passo: criar a função formatOpportunities(), que recebe essa lista a prepara para salvar na planilha, transformando em uma lista com os valores relevantes.

--- main.py
+++ main.py
@@ -375,6 +375,56 @@ def definePeriodo(diasAntes: int = 30, diasDepois: int = 15):
     except Exception as erro:
         log("errro", f"Ocorreu um erro: {erro}")
 
+
+def formatOpportunities(lista:list = []):
+    '''
+    Recebe uma lista de oportunidades e formata para salvar na planilha
+    '''
+
+def getOpportunities():
+    '''
+    Puxa todas as oportunidades de um determinado período
+
+    Puxa todas as oportunidaes criadas até 90 dias antes e salva em uma lista
+
+    Retorna uma lista com as oportunidades.
+    '''
+
+    try:
+        # Define os parâmetros da consulta
+        url = "https://services.leadconnectorhq.com/opportunities/search"
+        params = {
+            "location_id": locationId
+        }
+
+        header = {
+            "Authorization": f"Bearer {authorization}",
+            "Version": "2021-07-28"
+        }          
+
+        # Faz a requisição
+        r = requests.get(url=url, params=params, headers=header)  
+
+        # Se a resposta tiver um status code de erro, raise_for_status() vai disparar uma exceção HTTPError
+        r.raise_for_status()
+
+        # Transforma em JSON
+        r = r.json()
+
+        formatOpportunities()
+
+    except requests.exceptions.HTTPError as http_err:
+        # Exemplo: tratamento específico para token expirado (status 401)
+        if r.status_code == 401:
+            log("erro", "Erro 401: Token expirado.")
+        else:
+            log("erro",f"Erro HTTP: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        log("erro ",f"Erro na requisição: {req_err}")
+    except Exception as e:
+        log("erro",f"Ocorreu um erro inesperado: {e}")
+
+
 def getOpportunity(contactId: int = None):
 
     # Se não receber o ContactId, retorna "Não Encontrado"
@@ -504,8 +554,6 @@ def formatEvents(eventos:list = []):
 
     >>> formatEvents(getCalendarEvents())
     '''
-    from sheets import adicionarAgendamento
-
     # Deixa apenas os eventos
     eventos = eventos["events"]
 
@@ -553,17 +601,18 @@ def formatEvents(eventos:list = []):
         
 
 def main():
-    inicio = datetime.now()
-    log("sucesso", f"Aplicação iniciada às {inicio.strftime("%H:%M:%S")}")
-
-    authGHL()
-    formatEvents(getCalendarEvents())
-
-    fim = datetime.now()
-    duracao = fim - inicio
-    
-    log("sucesso", "Aplicação finalizada com êxito")
-    log(obs= f"Duração da execução: {duracao}")
+#     inicio = datetime.now()
+#     log("sucesso", f"Aplicação iniciada às {inicio.strftime("%H:%M:%S")}")
+# 
+#     authGHL()
+#     formatEvents(getCalendarEvents())
+# 
+#     fim = datetime.now()
+#     duracao = fim - inicio
+#     
+#     log("sucesso", "Aplicação finalizada com êxito")
+#     log(obs= f"Duração da execução: {duracao}")
+    getOpportunities()
 
 
 if __name__ == "__main__":
