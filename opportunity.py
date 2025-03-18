e2791a61aabe84fb17a4744e58a089c8a2e18c82
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-03-18 07:50:01 -0300

    oportunidades

--- opportunity.py
+++ opportunity.py
@@ -1,9 +1,48 @@
 from main import log, locationId, authorization, requests
 
+def diasAntes(n:int = None):
+    '''
+    Formata o dia no padrão mm-dd-yyyy
+
+    Recebe o dia de início da consulta.
+
+    Ex:
+
+    >>> diasAntes(60)
+
+    Retorna o período de até 60 dias antes
+
+    Se não informar nenhum valor, começará em 01/01/2023
+
+    '''
+    from datetime import datetime, timedelta
+
+    if n:
+        hoje = datetime.now()
+        diasAntes = hoje - timedelta(days=n)
+        diasAntes = diasAntes.strftime("%m-%d-%Y")
+        return(diasAntes)
+    
+    else:
+        return("01-01-2023")
+
+
 def formatOpportunities(lista:list = []):
     '''
     Recebe uma lista de oportunidades e formata para salvar na planilha
     '''
+
+    for op in lista:
+        
+        formatedOp = [
+        op.get("id", ""),
+        op.get("name", ""),
+        op.get("monetaryValue"),
+        
+        ]
+        print(op)
+
+   
 
 def getOpportunities():
     '''
@@ -15,28 +54,42 @@ def getOpportunities():
     '''
 
     try:
+
+        # Define uma lista de Oportunidades
+        ops = []
+
+        # Define a data do início da consulta
+        initialDate = diasAntes(1)
+
         # Define os parâmetros da consulta
-        url = "https://services.leadconnectorhq.com/opportunities/search"
-        params = {
-            "location_id": locationId
-        }
+        url = f"https://services.leadconnectorhq.com/opportunities/search?location_id={locationId}&limit=100&date={initialDate}"
 
         header = {
             "Authorization": f"Bearer {authorization}",
             "Version": "2021-07-28"
-        }          
+        } 
 
-        # Faz a requisição
-        r = requests.get(url=url, params=params, headers=header)  
+        while url:
+                     
+            # Faz a requisição
+            r = requests.get(url=url, headers=header)  
 
-        # Se a resposta tiver um status code de erro, raise_for_status() vai disparar uma exceção HTTPError
-        r.raise_for_status()
+            # Se a resposta tiver um status code de erro, raise_for_status() vai disparar uma exceção HTTPError
+            r.raise_for_status()
 
-        # Transforma em JSON
-        r = r.json()
+            # Transforma em JSON
+            r = r.json()
+
+            # Adiciona as oportunidades na lista
+            ops.extend(r["opportunities"])
+
+            # Redefine a URL para a próxima página
+            url = r["meta"]["nextPageUrl"]
 
         # Envia a lista para ser formatada
-        formatOpportunities(r)
+        formatOpportunities(ops)
+
+
 
     except requests.exceptions.HTTPError as http_err:
         # Exemplo: tratamento específico para token expirado (status 401)
@@ -48,3 +101,5 @@ def getOpportunities():
         log("erro ",f"Erro na requisição: {req_err}")
     except Exception as e:
         log("erro",f"Ocorreu um erro inesperado: {e}")
+
+getOpportunities()
