e7fcfbe083a3e128526342d7576d2ceea09d4d12
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-03-25 09:07:18 -0300

    Remodularização
    Desencanei da ideia de separar em várias pastas, complica muito para importar.
    
    Vou fazer um arquivo para cada coisa (eventos, oportunidades e contatos) e uma grande função em cada.
    
    Não preciso dividir cada um em "puxar dados", "formatar dados" e "salvar dados", posso fazer tudo em uma só função

--- V2/main.py
+++ V2/main.py
@@ -1,9 +1,9 @@
 from logging_config import logging
 from auth import ghlAuthorization
-from meetings.meetings import pegarSalvarReuniões
+from meetings import getMeetings
 
 if __name__ == "__main__":
     logging.info("Aplicação Iniciada")
     # Faz a autenticação com o GHL
     ghlAuthorization()
-    pegarSalvarReuniões()
+    getMeetings()
--- V2/meetings.py
+++ V2/meetings.py
@@ -1,28 +1,26 @@
+from logging_config import logging
 from requests import get
 from os import getenv
 from dotenv import load_dotenv
 from logging_config import logging
-import sys
-from pathlib import Path
-
-# Adiciona o diretório raiz ao PYTHONPATH
-sys.path.append(str(Path(__file__).parent.parent))
-
 from functions.date import definePeriodo
 
+
 def getMeetings():
-    '''
-    Faz a requisição na API e retorna o JSON
-    '''
     try:
+        # Carrega as variáveis de ambiente
         load_dotenv()
 
         calendarId = getenv("GHL_CALENDAR_ID")
         locationId = getenv("GHL_LOCATION_ID")
         authorization = getenv("GHL_AUTHORIZATION")
 
+
+        # Define o período da requisição
         startTime, endTime = definePeriodo()
 
+
+        # Requisição HTTP
         url = "https://services.leadconnectorhq.com/calendars/events/appointments/{eventId}"
 
         params = {
@@ -38,8 +36,17 @@ def getMeetings():
         }
 
         r = get(url=url, params=params, headers=header)
-        r.raise_for_status()
-        return r.json()
+
+
+        # Verifica se a requisição foi bem sucedida
+        if r.status_code != 200:
+            logging.error(f"Ocorreu um erro na requisição das reuniões: {r.text}")
+            return None
+
+
+        # Transforma em JSON
+        r = r.json()
+
 
     except Exception as e:
         logging.error(f"Ocorreu um erro: {e}")
